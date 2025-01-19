5e314dffc60a2a267c3b08f6cb1a2c324dea5bcd
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-01-19 12:55:14 +0530

    created the register component

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	return (
+		<div className='bg-blue-50 h-screen flex items-center justify-center'>
+			<form className="w-64 mx-auto mb-12">
+				<input value={username} onChange={e => setUsername(e.target.value)} type="text" placeholder="username" className="block w-full rounded-sm mb-2 p-2 border" />
+				<input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="password" className="block w-full rounded-sm mb-2 p-2 border" />
+				<button className="bg-blue-500 text-white w-full block rounded-sm p-2">Register</button>
+			</form>
+		</div> 
+	);
+};
+
+export default Register;
