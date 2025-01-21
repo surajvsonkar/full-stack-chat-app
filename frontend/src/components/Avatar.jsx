a62e5968f341c5fe9edb588e42a5a2cbdbeefd03
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-01-21 15:18:30 +0530

    added the logo component and added a feature to select a specific conversation

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Avatar = ({userId}) => {
+const Avatar = ({userId,username}) => {
+    const colors = ['bg-red-200', 'bg-green-200', 'bg-purple-200', 'bg-blue-200', 'bg-yellow-200', 'bg-teal-200']
+    const userIdBase10 = parseInt(userId, 15)
+    const colorIndex = userIdBase10 % colors.length
+    const color = colors[colorIndex]
+    console.log(color)
   return (
-    <div className='w-8 h-8 bg-red-300 rounded-full text-xl flex justify-center'>
-        {userId[0]}
+    <div className={`w-8 h-8 ${color} rounded-full text-xl flex justify-center opacity-70`}>
+        {username[0]}
     </div>
   )
 }
--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,62 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import Avatar from './Avatar';
+import Logo from './Logo';
 
 const Chat = () => {
-    const [ws, setWs] = useState(null)
-	const [onlinePeople, setOnlinePeople] = useState({})
-    useEffect(()=> {
-        const ws = new WebSocket('ws://localhost:3000')
-        setWs(ws)
-        ws.addEventListener('message', handleMessage)
+	const [ws, setWs] = useState(null);
+	const [onlinePeople, setOnlinePeople] = useState({});
+	const [selectedUser, setSelectedUser] = useState(null);
+	useEffect(() => {
+		const ws = new WebSocket('ws://localhost:3000');
+		setWs(ws);
+		ws.addEventListener('message', handleMessage);
 		// setInterval(()=> {
 		// 	const ws = new WebSocket('ws://localhost:3000')
 		// 	setWs(ws)
 		// 	ws.addEventListener('message', handleMessage)
 		// }, 3000)
-    }, [])
-    function showOnlinePeople(peopleArray) {
-        const people = {}
-		peopleArray.forEach(element => {
-			people[element.userId] = element.username
+	}, []);
+	function showOnlinePeople(peopleArray) {
+		const people = {};
+		peopleArray.forEach((element) => {
+			people[element.userId] = element.username;
 		});
-		setOnlinePeople(people)
-    }
-    function handleMessage(e) {
-        const messageData = JSON.parse(e.data)
-        if('online' in messageData) {
-            showOnlinePeople(messageData.online)
-        }
-    }
+		setOnlinePeople(people);
+	}
+	function handleMessage(e) {
+		const messageData = JSON.parse(e.data);
+		if ('online' in messageData) {
+			showOnlinePeople(messageData.online);
+		}
+	}
 	return (
 		<div className="flex h-screen">
 			<div className="bg-white w-1/3 pl-4 pt-4">
-			<div className='text-blue-700 font-bold uppercase hover:text-blue-500 flex gap-2 mb-4'>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-</svg>
-
-				MernChat
-			</div>
-				{Object.keys(onlinePeople).map((userId, key)=> {
-					return <div key={key}
-					className='border-b border-gray-100 py-2 flex gap-2 hover:border hover:border-gray-400 hover:bg-gray-200 p-2'
+				<Logo/>
+				{Object.keys(onlinePeople).map((userId, key) => {
+					return (
+					<div
+						key={key}
+						onClick={() => {
+							setSelectedUser(userId);
+							console.log(selectedUser);
+						}}
+						className={
+							'border-b border-gray-100 py-2 items-center cursor-pointer flex gap-2 hover:border hover:border-gray-400 hover:bg-gray-200 p-2' +
+							(selectedUser === userId ? ' bg-blue-200 hover:bg-blue-100' : '')
+						}
 					>
-					<Avatar userId={onlinePeople[userId]}/>
-					{onlinePeople[userId]}
+						<Avatar username={onlinePeople[userId]} userId={userId} />
+						<span>{onlinePeople[userId]}</span>
 					</div>
+					)
 				})}
 			</div>
 			<div className="flex flex-col bg-blue-50 w-2/3 p-2">
-				<div className='flex-grow'>
-                messages with selected person
-                </div>
-				<div className='flex gap-2 mx-2'>
+				<div className="flex-grow">messages with selected person</div>
+				<div className="flex gap-2 mx-2">
 					<input
 						type="text"
 						placeholder="type your message here"
 						className="bg-white rounded-sm flex-grow border p-2"
 					/>
-					<button className='bg-blue-500 rounded-sm p-2 text-white'>
+					<button className="bg-blue-500 rounded-sm p-2 text-white">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
